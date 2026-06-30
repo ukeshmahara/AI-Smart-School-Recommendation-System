99bdbc502c77db267b15ee3d040489063706e484
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-30 10:57:17 +0545

    Add update route protected by auth middleware with file upload

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 import { authorizedMiddleware } from "../middlewares/authorized.middleware";
+import { uploadMiddleware } from "../middlewares/upload.middleware";
 
 const userRouter = Router();
 const userController = new UserController();
@@ -8,5 +9,11 @@ const userController = new UserController();
 userRouter.post("/register", userController.createUser);
 userRouter.post("/login", userController.loginUser);
 userRouter.get("/whoami", authorizedMiddleware, userController.whoami);
+userRouter.patch(
+    "/update",
+    authorizedMiddleware,
+    uploadMiddleware.single("profileImage"),
+    userController.updateUser
+);
 
 export default userRouter;
