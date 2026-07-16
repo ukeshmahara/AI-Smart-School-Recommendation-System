9a751923d2bcd16fde63934f64ac13403ed6f71a
ukeshmahara <ukeshmahara361@gmail.com>
2026-07-16 11:50:29 +0545

    Added student and admin review routes

---
+++ backend/src/routes/review.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { ReviewController } from "../controllers/review.controller";
+import { authorizedMiddleware } from "../middlewares/authorized.middleware";
+
+const reviewRouter = Router();
+const reviewController = new ReviewController();
+
+reviewRouter.use(authorizedMiddleware);
+
+reviewRouter.post("/", reviewController.createReview);
+reviewRouter.get("/school/:schoolId", reviewController.getSchoolReviews);
+reviewRouter.get("/my", reviewController.getMyReviews);
+reviewRouter.patch("/:id", reviewController.updateReview);
+reviewRouter.delete("/:id", reviewController.deleteReview);
+
+export default reviewRouter;
